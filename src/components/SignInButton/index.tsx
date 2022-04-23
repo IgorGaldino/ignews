12ff5ee315ea845bce1ca6ecc389b14eaec1d65b
Igor Galdino <igormgaldino@gmail.com>
2022-04-23 12:25:30 -0300

    Added authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,36 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
-  return (
+  const { data: session } = useSession();
+
+  const formatName = (fullName) => {
+    if (!fullName) return "User";
+    const splitedName = fullName.split(" ");
+    return `${splitedName[0]} ${splitedName[splitedName.length - 1]}`
+  }
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
-      <FaGithub color={isUserLoggedIn ? "#04d361" : "#eba417"} />
-      {isUserLoggedIn ? "Igor Galdino" : "Sign in with Github"}
-      {isUserLoggedIn && (
-        <FiX color="#737380" className={styles.closeIcon} />
-      )}
+      <FaGithub color="#04d361" />
+      {formatName(session.user.name)}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with Github
     </button>
   );
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import { Console } from "console";
+import { Log, query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
+});
